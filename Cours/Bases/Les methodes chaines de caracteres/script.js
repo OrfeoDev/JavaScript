984fb0eea8a06893fe2be117665027cf14bd1ce7
OrfeoDev <orfe34@icloud.com>
2022-02-20 19:30:23 +0100

    " Les methodes de chaine de caratere"

---
+++ Cours/Bases/Les methodes chaines de caracteres/script.js
@@ -0,0 +1,22 @@
+// const numTab = [1, 2, 3, 56, 8, 89,];
+// for (let i = 0; i < numTab.length; i++){
+//     console.log(numTab[i]);
+// }
+
+ const  str = 'Lorem ipsum dolor sit amet'
+// permet de recuperer un caractere
+console.log(str.charAt(4));
+// Est ce que mon texte contient IPSUM
+console.log(str.includes('ipsum'));
+// son index de depart
+console.log(str.indexOf('dolor'))
+// c'est pour l'unicode par exemple pour savoir ce qu'a taper l'utilsateur
+console.log(str.charCodeAt(0));
+// permet de couper une chaine de caractere
+console.log(str.slice(3));
+// split va separer dans un tableau
+const strToArray = str.split(' ');
+console.log(strToArray)
+console.log(strToArray.join('    '))
+
+co
