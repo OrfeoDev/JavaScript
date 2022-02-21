993d11860e2911766ea53508ad0a46b0bad2c7ea
OrfeoDev <orfe34@icloud.com>
2022-02-21 11:07:32 +0100

    " Dom supprimer element"

--- les differentes selection du DOM/dom.js
+++ les differentes selection du DOM/dom.js
@@ -1,8 +1,22 @@
-const premierTitre= document.getElementsByClassName('premier-titre');
+const premierTitre= document.getElementsByClassName('.premier-titre');
 const premierID= document.getElementById('premier-id');
 const titreNu =document.querySelector('h3');
 const allItems =document.querySelectorAll('.item-liste');
+const  liste =document.querySelector('ul');
+const i2 = document.querySelector('.i2');
 
-console.log('premierTitre');
-console.log('premierID');
-console.log('h3');
+premierTitre.innerHTML= 'hello jjghkjlkjhggjhkjlkjkhjghf';
+ const parent = document.querySelector('.parent');
+ parent.innerHTML= '<h1> etexte</h1>';
+// Ici je cree un nouvel li
+ let nvEl = document.createElement('li');
+ // Ici j'ecris du texte a l'interieur
+ nvEl.innerText = " Nouvel Items";
+ // Ici je le rajoute a la fin
+//liste.appendChild(nvEl);
+// Ici je le met hors du ul. C'est a usage unique , on ne cree qu'un seul element
+// on ne peut pas faire document.body et liste appendchil
+document.body.appendChild(nvEl)
+
+//suppression d'un element
+liste.children[2].remove()
